29a9777c660aecca0f4e1658bf84bb4ebf6bce2f
Bradley Moore <bradleym007@gmail.com>
2018-03-21 08:53:29 -0700

    Added some more functionality

--- wp-hubspot-api.php
+++ wp-hubspot-api.php
@@ -187,6 +187,31 @@ if ( ! class_exists( 'HubSpotAPI' ) ) {
 			return ( 200 <= $code && 300 > $code );
 		}
 
+		/**
+		 * Takes an array of arguments, and 'parses' them for being null or not.
+		 *
+		 * Returns an array with the same keys and vals if the val is not null.
+		 *
+		 * Also optionally adds a 'merge' array, to merge into (array_merge( $merge, $results )).
+		 *
+		 * @param  [type] $args  [description]
+		 * @param  array  $merge [description]
+		 * @return [type]        [description]
+		 */
+		private function parse_args( $args, $merge = array() ){
+	    $results = array();
+
+	    foreach( $args as $key => $val ){
+	      if( $val !== null ){
+	        $results[$key] = $val;
+	      }else if( is_array( $val ) && ! empty( $val ) ){
+	        $results[$key] = $val;
+	      }
+	    }
+
+	    return array_merge( $merge, $results );
+	  }
+
 		/* Oauth. */
 
 
@@ -632,30 +657,51 @@ if ( ! class_exists( 'HubSpotAPI' ) ) {
 		 * @return array                    Array of contact info.
 		 */
 		function get_all_contacts( int $count = null, int $contact_offset = null, $property = null, string $property_mode = null, string $form_submit_mode = null, bool $list_memberships = null ) {
-			$args = array();
 
-			if ( null !== $count ) {
-				$args['count'] = $count;
-			}
-			if ( null !== $contact_offset ) {
-				$args['vidOffset'] = $contact_offset;
-			}
-			if ( null !== $property ) {
-				$args['property'] = $property;
-			}
-			if ( null !== $property_mode ) {
-				$args['propertyMode'] = $property_mode;
-			}
-			if ( null !== $form_submit_mode ) {
-				$args['formSubmissionMode'] = $form_submit_mode;
-			}
-			if ( null !== $list_memberships ) {
-				$args['showListMemberships'] = $list_memberships;
-			}
+			$args = $this->parse_args(array(
+				'count' => $count,
+				'vidOffset' => $contact_offset,
+				'property' => $property,
+				'propertyMode' => $property_mode,
+				'formSubmissionMode' => $form_submit_mode,
+				'showListMemberships' => $list_memberships
+			));
 
-			return $this->build_request( 'contacts/v1/lists/all/contacts/all', $args )->fetch();
+			return $this->run( 'contacts/v1/lists/all/contacts/all', $args );
 		}
 
+		/**
+		 * Get a single contact, by visitor_id (vid).
+		 *
+		 * $args supports additional all optional properties:
+		 *   Property
+		 *     Used in the request URL	By default, you will get all properties that
+		 *     the contact has values for. If you include the "property" parameter,
+		 *     then the returned data will only include the property or properties
+		 *     that you request. You can include this parameter multiple times to
+		 *     specify multiple properties. The lastmodifieddate and associatedcompanyid
+		 *     will always be included, even if not specified. Keep in mind that only
+		 *     properties that have a value will be included in the response, even if
+		 *     specified in the URL.
+		 *
+		 *     For our purposes, we accept both a string or an array of values under
+		 *     the key 'property'. ie: 'property' => 'imforza_user_id' or
+		 *     'property' => array( 'imforza_user_id', 'lastname', 'firstname' ).
+		 *   Property Mode
+		 *     One of “value_only” or “value_and_history” to specify if the current
+		 *     value for a property should be fetched, or the value and all the
+		 *     historical values for that property. Default is “value_and_history”.
+		 *   Form Submission Mode
+		 *     One of “all”, “none”, “newest”, “oldest” to specify which form submissions
+		 *     should be fetched. Default is “all”.
+		 *   List Memberships
+		 *     Boolean "true" or "false" to indicate whether current list memberships
+		 *     should be fetched for the contact. Default is true.
+		 *
+		 * @param  [type] $contact_id [description]
+		 * @param  array  $args       [description]
+		 * @return [type]             [description]
+		 */
 		function get_contact( $contact_id, $args = array() ) {
 			return $this->run( 'contacts/v1/contact/vid/' . $contact_id . '/profile', $args );
 		}
@@ -664,8 +710,64 @@ if ( ! class_exists( 'HubSpotAPI' ) ) {
 			return $this->run( 'contacts/v1/contact/email/' . $contact_email . '/profile' );
 		}
 
-		function get_recent_contacts() {
-			// https://api.hubapi.com/contacts/v1/lists/recently_updated/contacts/recent?hapikey=demo&count=2
+		/**
+		 * Get recently updated and created contacts
+		 *
+		 * For a given portal, return all contacts that have been recently updated or
+		 * created.
+		 *
+		 * A paginated list of contacts will be returned to you, with a maximum of 100
+		 * contacts per page, as specified by the "count" parameter. The endpoint only
+		 * scrolls back in time 30 days.
+		 *
+		 * Please Note There are 3 fields here to pay close attention to: the "has-more"
+		 * field that will let you know whether there are more contacts that you can pull
+		 * from this portal, and the "vid-offset" and "time-offset" fields which will
+		 * let you know where you are in the list of contacts. You can then use the
+		 * "vid-offset" and "time-offset" fields in the "vidOffset" and "timeOffset"
+		 * parameters described below.
+		 *
+		 * The response is sorted in descending order by last modified date; the most
+		 * recently modified record is returned first.
+		 *
+		 * $args accepts all optional values in a key => value form:
+		 *   count:
+		 *     Used in the request URL	This parameter lets you specify the amount of
+		 *     contacts to return in your API call. The default for this parameter
+		 *     (if it isn't specified) is 20 contacts. The maximum amount of contacts
+		 *     you can have returned to you via this parameter is 100.
+		 *   timeOffset:
+		 *     Used in the request URL	Used in conjunction with the vidOffset paramter
+		 *     to page through the recent contacts. Every call to this endpoint will
+		 *     return a time-offset value. This value is used in the timeOffset
+		 *     parameter of the next call to get the next page of contacts.
+		 *   vidOffset:
+		 *     Used in the request URL	Used in conjunction with the timeOffset paramter
+		 *     to page through the recent contacts. Every call to this endpoint will
+		 *     return a vid-offset value. This value is used in the vidOffset parameter
+		 *     of the next call to get the next page of contacts.
+		 *   property:
+		 *     Used in the request URL	If you include the "property" parameter, then
+		 *     the properties in the "contact" object in the returned data will only
+		 *     include the property or properties that you request.
+		 *
+		 *     For our purposes, we accept an array or a single value. See get_all_contacts.
+		 *   propertyMode:
+		 *     One of “value_only” or “value_and_history” to specify if the current
+		 *     value for a property should be fetched, or the value and all the
+		 *     historical values for that property. Default is “value_only”.
+		 *   formSubmissionMode:
+		 *     One of “all”, “none”, “newest”, “oldest” to specify which form submissions
+		 *     should be fetched. Default is “newest”.
+		 *   showListMemberships:
+		 *     Boolean "true" or "false" to indicate whether current list memberships
+		 *     should be fetched for the contact. Default is false.
+		 *
+		 * @param  array  $args [description]
+		 * @return [type]       [description]
+		 */
+		function get_recent_contacts( $args = array() ) {
+			return $this->run( 'contacts/v1/lists/recently_updated/contacts/recent', $args );
 		}
 
 		function get_contact_by_token( $contact_token ) {
