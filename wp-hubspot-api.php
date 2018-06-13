e1e22fc0cb09dd9dffaa88e929f81f6baaa718d6
Bradley Moore <bradleym007@gmail.com>
2018-06-13 12:23:11 -0700

    Ugh

--- wp-hubspot-api.php
+++ wp-hubspot-api.php
@@ -113,14 +113,14 @@ if ( ! class_exists( 'HubSpotAPI' ) ) {
 			// Generate query string for GET requests.
 			if ( 'GET' === $method ) {
 				$this->route = add_query_arg( array_filter( $args ), $this->route );
-
-				// Hubspot api is jank and doesnt use proper URL encode standards... So we must jank it up.
-				$this->route = preg_replace( '/\%5B\d+\%5D/', '', $this->route );
 			} elseif ( 'application/json' === $this->args['headers']['Content-Type'] ) {
 				$this->args['body'] = wp_json_encode( $args );
 			} else {
 				$this->args['body'] = $args;
 			}
+
+			// Hubspot api is jank and doesnt use proper URL encode standards... So we must jank it up.
+			$this->route = preg_replace( '/\%5B\d+\%5D/', '', $this->route );
 
 			return $this;
 		}
