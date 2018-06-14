7044042dd8e10e0159c8c726cea41a167b6f6834
Santiago Garza <santiago.garza@imforza.com>
2018-06-14 11:31:46 -0700

    Timeline API routes added

--- wp-hubspot-api.php
+++ wp-hubspot-api.php
@@ -1609,40 +1609,132 @@ if ( ! class_exists( 'HubSpotAPI' ) ) {
 
 		/* Timeline. */
 
-		function add_or_update_timeline_event() {
-
+		function add_or_update_timeline_event( $app_id, $args ) {
+			return $this->run( "integrations/v1/$app_id/timeline/event", $args, 'PUT' );
 		}
 
-		function get_timeline_event_types() {
-
+		/**
+		 * [get_timeline_event description]
+		 *
+		 * @see https://developers.hubspot.com/docs/methods/timeline/get-event   Documentation
+		 * @param  [type] $app_id        [description]
+		 * @param  [type] $event_type_id [description]
+		 * @param  [type] $event_id      [description]
+		 * @return [type]                [description]
+		 */
+		function get_timeline_event( $app_id, $event_type_id, $event_id ){
+			return $this->run( "integrations/v1/$app_id/timeline/event/$event_type_id/$event_id" );
 		}
 
-		function add_new_timeline_event_type() {
-
+		/**
+		 * [get_timeline_event_types description]
+		 *
+		 * @see https://developers.hubspot.com/docs/methods/timeline/get-event-types   Documentation
+		 * @return [type] [description]
+		 */
+		function get_timeline_event_types( $app_id ) {
+			return $this->run( "integrations/v1/$app_id/timeline/event-types" );
 		}
 
-		function update_timeline_event_type() {
+		function add_new_timeline_event_type( $app_id, $user_id, $name, $args = array() ) {
+			$args['applicationId'] = $app_id;
+			$args['name'] = $name;
 
+			$url = "integrations/v1/$app_id/timeline/event-types";
+			$url = add_query_arg( 'userId', $user_id, $url );
+
+			return $this->run( $url, $args, 'POST' );
 		}
 
-		function delete_timeline_event_type() {
-
+		/**
+		 * [update_timeline_event_type description]
+		 *
+		 * @see https://developers.hubspot.com/docs/methods/timeline/update-event-type Documentation
+		 * @param  [type] $app_id  [description]
+		 * @param  [type] $user_id [description]
+		 * @param  [type] $name    [description]
+		 * @param  array  $args    [description]
+		 * @return [type]          [description]
+		 */
+		function update_timeline_event_type( $app_id, $event_type_id, $args = array() ) {
+			return $this->run( "integrations/v1/$app_id/timeline/event-types/$event_type_id", $args, 'PUT' );
 		}
 
-		function get_properties_for_timeline_event_type() {
 
+		/**
+		 * [delete_timeline_event_type description]
+		 *
+		 * @see https://developers.hubspot.com/docs/methods/timeline/delete-event-type Documentation
+		 * @param  [type] $app_id        [description]
+		 * @param  [type] $event_type_id [description]
+		 * @param  array  $user_id       [description]
+		 * @return [type]                [description]
+		 */
+		function delete_timeline_event_type( $app_id, $event_type_id, $user_id  ) {
+			return $this->run( add_query_arg( 'userId', $user_id,"/integrations/v1/$app_id/timeline/event-types/$event_type_id" ), array(), 'DELETE' );
 		}
 
-		function add_property_for_timeline_event_type() {
-
+		/**
+		 * [get_properties_for_timeline_event_type description]
+		 *
+		 * @see https://developers.hubspot.com/docs/methods/timeline/get-timeline-event-type-properties Documentation
+		 * @param  [type] $app_id        [description]
+		 * @param  [type] $event_type_id [description]
+		 * @return [type]                [description]
+		 */
+		function get_properties_for_timeline_event_type($app_id, $event_type_id) {
+			return $this->run( "integrations/v1/$app_id/timeline/event-types/$event_type_id/properties" );
 		}
 
-		function update_property_for_timeline_event_type() {
+		/**
+		 * [add_property_for_timeline_event_type description]
+		 *
+		 * @see https://developers.hubspot.com/docs/methods/timeline/create-timeline-event-type-property Documentation
+		 * @param [type] $app_id        [description]
+		 * @param [type] $event_type_id [description]
+		 * @param [type] $name          [description]
+		 * @param [type] $label         [description]
+		 * @param [type] $property_type [description]
+		 * @param array  $args          [description]
+		 */
+		function add_property_for_timeline_event_type( $app_id, $event_type_id, $name, $label, $property_type, $args = array() ) {
 
+			$args['name'] = $name;
+			$args['label'] = $label;
+			$args['propertyType'] = $property_type;
+
+			return $this->run( "integrations/v1/$app_id/timeline/event-types/$event_type_id/properties", $args, "POST" );
 		}
 
-		function delete_property_for_timeline_event_type() {
+		/**
+		 * [update_property_for_timeline_event_type description]
+		 *
+		 * @see https://developers.hubspot.com/docs/methods/timeline/udpate-timeline-event-type-property Documentation
+		 *
+		 * @param  [type] $app_id        [description]
+		 * @param  [type] $event_type_id [description]
+		 * @param  [type] $prop_id       [description]
+		 * @param  [type] $args          [description]
+		 * @return [type]                [description]
+		 */
+		function update_property_for_timeline_event_type( $app_id, $event_type_id, $prop_id, $args = array() ) {
+			$args['id'] = $prop_id;
 
+			return $this->run( "integrations/v1/$app_id/timeline/event-types/$event_type_id/properties", $args, "PUT" );
+		}
+
+		/**
+		 * [delete_property_for_timeline_event_type description]
+		 *
+		 * @see https://developers.hubspot.com/docs/methods/timeline/delete-timeline-event-type-property Documentation
+		 *
+		 * @param  [type] $app_id        [description]
+		 * @param  [type] $event_type_id [description]
+		 * @param  [type] $prop_id       [description]
+		 * @return [type]                [description]
+		 */
+		function delete_property_for_timeline_event_type($app_id, $event_type_id, $prop_id) {
+			return $this->run( "integrations/v1/$app_id/timeline/event-types/$event_type_id/properties/$prop_id", array(), "DELETE" );
 		}
 
 		/**
